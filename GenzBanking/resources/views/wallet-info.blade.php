06c88003b5c51f13dd50c3346885465c25b775e4
Niamul <chowdhuryniamul19@gmail.com>
2025-04-18 03:40:31 +0600

    slight changes in json

--- GenzBanking/resources/views/wallet-info.blade.php
+++ GenzBanking/resources/views/wallet-info.blade.php
@@ -5,6 +5,7 @@
     <div class="card-body">
         <p><strong>Wallet Holder:</strong> {{ auth()->user()->name }}</p>
         @if(auth()->user()->wallet)
+            
             <p><strong>Balance:</strong> ${{ number_format(auth()->user()->wallet->balance, 2) }}</p>
         @else
             <p><strong>Balance:</strong> $0.00</p>
